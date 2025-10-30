65a1cf494e301ea1d3474324e35efb39e898fc38
danielbloy <daniel_bloy@hotmail.com>
2025-10-30 20:52:26 +0000

    Added comment

--- desktop/background/launcher.sh
+++ desktop/background/launcher.sh
@@ -1,3 +1,4 @@
+# From: https://www.instructables.com/Raspberry-Pi-Launch-Python-script-on-startup/
 cd /home/daniel/pico-interactive-halloween
 source audio_venv/bin/activate
 export PYTHONPATH=/home/daniel/pico-interactive
